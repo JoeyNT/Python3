bc5a19e8d9dbad6e5bbfc5aa08efc18af4b8e2e9
Joey Cuomo <48045756+JoeyNT@users.noreply.github.com>
2023-11-08 19:51:00 -0800

    Create IDsGenerator.bash

---
+++ IDsGenerator.bash
@@ -0,0 +1,13 @@
+# Assign the loop variable `i` to an initial value of 5000
+
+i = 5000
+
+# While loop that generates unique employee IDs for the Sales department by iterating through numbers
+# and displays each ID created
+# This loop displays "Only 10 valid employee ids remaining" once `i` reaches 5100
+
+while i <= 5150: 
+    print(i)
+    if i == 5100
+        print("Only 10 valid employee ids remaining")
+    i = i + 5
